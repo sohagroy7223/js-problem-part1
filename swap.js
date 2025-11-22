e6fe3bf15c6f0f7605fda4c6efc7f9185343cb1f
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-22 23:36:20 +0600

    addad swap to js function

---
+++ swap.js
@@ -0,0 +1,21 @@
+let a = 8;
+let b = 10;
+// console.log(a, b);
+// a = b;
+// b = a;
+// console.log(a, b);
+//----------------------------
+let x = 7;
+let y = 9;
+console.log(x, y);
+const temp = x;
+x = y;
+y = temp;
+console.log(x, y);
+console.log("-------------");
+
+let p = 5;
+let q = 6;
+console.log(p, q);
+[p, q] = [q, p];
+console.log(p, q);
