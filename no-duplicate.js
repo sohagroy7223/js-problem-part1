417556a46e8ea6d048ce331293913cd347e43544
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-22 12:39:28 +0600

    addad duplicate for of and for loop in function

---
+++ no-duplicate.js
@@ -0,0 +1,54 @@
+function noDuplicate(array) {
+    const update = [];
+    for (const item of array) {
+        if (update.includes(item) === false) {
+            update.push(item);
+        }
+    }
+    return update;
+}
+const item = [12, 35, 68, 12, 45, 87, 12, 45, 35];
+// console.log(noDuplicate(item));
+
+function noDuplicate1(array) {
+    const duplicate = [];
+    for (const item of array) {
+        if (duplicate.includes(item) === false) {
+            duplicate.push(item);
+        }
+    }
+    return duplicate;
+}
+const mistyKhor = [
+    "saikot",
+    "sumon",
+    "somu",
+    "samor",
+    "sumon",
+    "somu",
+    "sumon",
+    "sagor",
+];
+// console.log(noDuplicate1(mistyKhor));
+
+// function teaKhor(array) {
+//     for (a = 0; a < array.length; a++) {
+//         console.log(a);
+//     }
+// }
+// const manus = ["elon", "bil", "sumon", "sagor", "elon", "bil"];
+// console.log(teaKhor(manus));
+
+function noDuplicate(array) {
+    const unique = [];
+    for (let i = 0; i < array.length; i++) {
+        if (unique.includes(array[i]) === false) {
+            unique.push(array[i]);
+        }
+    }
+    return unique;
+}
+
+const manus = ["elon", "bil", "sumon", "sagor", "elon", "bil"];
+
+console.log(noDuplicate(manus));
